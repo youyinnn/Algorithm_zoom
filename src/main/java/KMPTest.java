09c12b010c8862182fb54ea6743bce225e795dff
youyinnn <454947049@qq.com>
2018-08-24 16:27:50 +0800

    IMPMODU
    IMPMODU:
    1.kmp nextVal

--- src/main/java/KMPTest.java
+++ src/main/java/KMPTest.java
@@ -1,3 +1,7 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * @author youyinnn
  * Date 8/23/2018
@@ -7,13 +11,19 @@ public class KMPTest {
     public static void main(String[] args) {
         String s = "asdasde";
         String p = "asde";
-        System.out.println(bruteForceStringMatch(s, p));
-        System.out.println(bruteForceStringMatch2(s, p));
-        System.out.println(kmp(s, p));
+        System.out.println(bruteForceStringMatch(s.toCharArray(), p.toCharArray()));
+        System.out.println(bruteForceStringMatch2(s.toCharArray(), p.toCharArray()));
+        System.out.println(kmpA(s.toCharArray(), p.toCharArray()));
     }
 
-    public static int[] next(String pattern) {
-        char[] p = pattern.toCharArray();
+    @Test
+    public void testNext(){
+        String p = "ababaabab";
+        System.out.println(Arrays.toString(next(p.toCharArray())));
+        System.out.println(Arrays.toString(nextVal(p.toCharArray())));
+    }
+
+    public static int[] next(char[] p) {
         int[] next = new int[p.length];
         next[0] = -1;
         int j = 0;
@@ -30,15 +40,34 @@ public class KMPTest {
         return next;
     }
 
-    public static int kmp(String text, String pattern) {
-        char[] t = text.toCharArray();
-        char[] p = pattern.toCharArray();
+    public static int[] nextVal(char[] p) {
+        int[] nextVal = new int[p.length];
+        int j = 0;
+        int k = -1;
+        nextVal[0] = -1;
+        while (j < p.length - 1) {
+            if (k == -1 || p[j] == p[k]) {
+                j++;
+                k++;
+                if (p[j] != p[k]) {
+                    nextVal[j] = k;
+                } else {
+                    nextVal[j] = nextVal[k];
+                }
+            } else {
+                k = nextVal[k];
+            }
+        }
+        return nextVal;
+    }
+
+    public static int kmpA(char[] t, char[] p) {
         if (t.length < p.length) {
             return -1;
         } else if (t.length == p.length && t[0] != p[0]) {
             return -1;
         }
-        int[] next = next(pattern);
+        int[] next = next(p);
         int i = 0;
         int j = 0;
         while (i < t.length && j < p.length) {
@@ -59,9 +88,7 @@ public class KMPTest {
         }
     }
 
-    public static int bruteForceStringMatch(String text, String pattern){
-        char[] t = text.toCharArray();
-        char[] p = pattern.toCharArray();
+    public static int bruteForceStringMatch(char[] t, char[] p) {
         int n = t.length;
         int m = p.length;
         for (int i = 0; i <= n - m; i++){
@@ -76,9 +103,7 @@ public class KMPTest {
         return -1;
     }
 
-    public static int bruteForceStringMatch2(String text, String pattern) {
-        char[] t = text.toCharArray();
-        char[] p = pattern.toCharArray();
+    public static int bruteForceStringMatch2(char[] t, char[] p) {
         // 主串的位置
         int i = 0;
         // 模式串的位置
