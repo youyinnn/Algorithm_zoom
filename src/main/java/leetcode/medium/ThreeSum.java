10b666e8392db55ebb63ae62680dc12b3d3f935f
youyinnn <454947049@qq.com>
2019-03-07 23:33:03 +0800

    sums

---
+++ src/main/java/leetcode/medium/ThreeSum.java
@@ -0,0 +1,48 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author youyinnn
+ * Date 3/7/2019
+ */
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int l = i+1, r = nums.length - 1, need = 0 - nums[i];
+            while(l < r){
+                if(nums[l] + nums[r] == need) {
+                    res.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+                    while(r > l && nums[l] == nums[l-1]) {
+                        l++;
+                    }
+                    while(r > l && nums[r] == nums[r+1]) {
+                        r--;
+                    }
+                }else if(nums[l] + nums[r] > need){
+                    r--;
+                }else{
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+
+}
---
+++ src/main/java/leetcode/medium/ThreeSumClosest.java
@@ -0,0 +1,68 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author youyinnn
+ * Date 3/7/2019
+ */
+public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Integer closest = null;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == target) {
+                        closest = sum;
+                    }
+                    int dis = Math.abs(sum - target);
+                    if (closest == null) {
+                        closest = sum;
+                    } else if (dis < Math.abs(closest - target)) {
+                        closest = sum;
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+
+    @Test
+    public void test1(){
+        System.out.println(threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+    }
+
+    public int threeSumClosest2(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > target) {
+                    r--;
+                } else {
+                    l++;
+                }
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+            }
+        }
+        return closest;
+    }
+
+    private int distance(int target, int input) {
+        return Math.abs(input - target);
+    }
+
+    @Test
+    public void test2(){
+        //System.out.println(threeSumClosest2(new int[]{0,2,1,-3}, 1));
+        System.out.println(threeSumClosest2(new int[]{-1,2,1,-4}, 1));
+    }
+}
